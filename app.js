fbd3b147aae591de0e0f0c3b2593bab4ac591b18
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-05-24 15:03:05 +0200

    Creato posts con controller

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+// Vari require e const importanti
 const express = require('express');
 const app = express();
 const port = 3000
+    // Prendo il file di posts
+    const posts = require("./controllers/posts.js");
 
+// Pagina benvenuto
 app.get('/', (req, res) => {
     res.send('<h1>Benvenuto nel mio blog!</h1>')
-
 })
 
+// Posts
+app.get("/posts", posts.index)
+
 app.listen(port, () => {
-    console.log("Prova pippo2")
 })
---
+++ controllers/posts.js
@@ -0,0 +1,5 @@
+module.exports.index = function(req, res) {
+  res.send("<h1>Benvenuto nell'angolo rotture della lidl</h1>")
+  }
+
+
